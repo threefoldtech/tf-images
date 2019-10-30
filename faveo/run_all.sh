6a519d88fb370a908d094e4b1408a9342cdf7da4
hossnys <hossnys@greenitglobe.com>
2019-10-30 14:41:31 +0200

    Faveo

---
+++ faveo/run_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+chmod +x /opt/bin/*
+
+echo "runing mariadb"
+/bin/bash /opt/bin/mariadb_entry.sh mysqld&  #--character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci& #--user=root&
+
+
+while ! ss -ntlp | grep -q '3306'; do
+ ss -ntlp
+ echo "waiting for mariab"
+ sleep 10;
+done
+
+
+echo "mariaserver running"
+echo "running wordpress entry"
+
+
+exec "$@"
