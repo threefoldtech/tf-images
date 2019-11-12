68fa5d026ab55ef304ecb2d5e05da4a527184b14
Bishoy <abdelmab@codescalers.com>
2019-11-12 16:33:59 +0200

    change service to be run by supervisord

--- odoo/entrypoint.sh
+++ odoo/entrypoint.sh
@@ -9,13 +9,24 @@ chown postgres:ssl-cert /etc/ssl/private
 chown -R postgres /var/run/postgresql
 chown -R postgres.postgres /etc/postgresql
 
+if [ -d /var/run/postgresql ]; then
+    chmod 2775 /var/run/postgresql
+else
+    install -d -m 2775 -o postgres -g postgres /var/run/postgresql
+fi
+
 # prepare odoo
 chown odoo /etc/odoo/odoo.conf
 chown -R odoo.odoo /var/lib/odoo
 chown -R odoo /mnt/extra-addons
-su -c "createuser -s odoo" postgres || true
 chown -R postgres.postgres /supervisor/logs/postgresql
 chown -R odoo.odoo /supervisor/logs/odoo
 
 # use supervisord to start ssh, postgres and odoo
 supervisord -c /etc/supervisor/supervisord.conf
+echo "waiting 3 second to start postgres"
+sleep 3
+echo "create a odoo user in db"
+su -c "createuser -s odoo" postgres || true
+
+exec "$@"
