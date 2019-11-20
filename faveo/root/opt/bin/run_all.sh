693d554a7b2c42a408b2ad1272a0f5dcc9aba140
Bishoy <you@example.com>
2019-11-20 09:52:33 +0200

    fix starting mysql

--- faveo/root/opt/bin/run_all.sh
+++ faveo/root/opt/bin/run_all.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
 chmod +x /opt/bin/*
-
 echo "runing mariadb"
 /bin/bash /opt/bin/mariadb_entry.sh
-su mysql -c "mysqld &"  #--character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci& #--user=root&
-
-
-while ! ss -ntlp | grep -q '3306'; do
- ss -ntlp
- echo "waiting for mariab"
- sleep 10;
-done
 
 echo "mariaserver running"
 echo "running faveo server"
@@ -28,9 +19,7 @@ mkdir -p /run/sshd
 
 chown -R www-data:www-data /var/log/supervisor
 chmod -R 0777 /usr/share/nginx/storage
-echo '* * * * * php /usr/share/nginx/artisan schedule:run > /dev/null 2>&1' > /etc/cron.d/faveo-cron
 chmod 0644 /etc/cron.d/faveo-cron
-crontab /etc/cron.d/faveo-cron
 
 # use supervisord to start ssh, mysql and nginx and php-fpm
 
