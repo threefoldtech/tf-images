eec63d76030f108edc220b6d9de4f8f1a0da33e8
Bishoy <bishoy@incubaid.com>
2020-01-26 14:57:06 +0000

    fix starting rake

--- discourse/start_discourse.sh
+++ discourse/start_discourse.sh
@@ -83,6 +83,16 @@ gem update bundler
 find $home ! -user discourse -exec chown discourse {} \+
 cd $home
 su discourse -c 'bundle install --deployment --retry 3 --jobs 4 --verbose --without test development'
+DEV_RAKE='/var/www/discourse/vendor/bundle/ruby/2.6.0/gems/railties-6.0.1/lib/rails/tasks/dev.rake'
+
+if [[ -f $DEV_RAKE ]] ;then
+	echo " $DEV_RAKE file is exist "
+else
+	echo " $DEV_RAKE file does not exist "
+	cp /.dev.rake $DEV_RAKE
+	chown discourse:discourse $DEV_RAKE
+
+fi
 su discourse -c 'bundle exec rake db:migrate'
 su discourse -c 'bundle exec rake assets:precompile'
 
