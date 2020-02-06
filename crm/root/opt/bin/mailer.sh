9c8d22b1ee83e80f49d4814cb919e6d685faf49e
Bishoy <abdelmab@codescalers.com>
2020-02-06 16:42:03 +0200

    add crm

---
+++ crm/root/opt/bin/mailer.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo **START**;cd /opt/code/github/incubaid/crm;export SQLALCHEMY_DATABASE_URI=postgresql://postgres:postgres@localhost:5432/crm;export ENV=prod;export FLASK_APP=app.py;export  CACHE_BACKEND_URI=redis://127.0.0.1:6379/0;flask mailer && echo **OK** || echo **ERROR**
