4ee3408cf5357845a624bf2b67014dc3202ecc6b
Bishoy <abdelmab@codescalers.com>
2020-02-12 12:48:25 +0200

    add supervisor

--- gitea/root/opt/bin/run_all.sh
+++ gitea/root/opt/bin/run_all.sh
@@ -5,14 +5,14 @@ chmod +x /opt/bin/*
 
 echo "runing postgres"
 /bin/bash /opt/bin/postgres_entry.sh postgres
-su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
+mkdir -p /var/log/{postgres,gitea,cron}
 
 # su git -c "gitea create-user --username=$ADMIN_USER --password=$ِADMIN_PASSWORD --email=$ADMIN_EMAIL --admin"&
 
-sleep 4
-echo "running gitea entry"
+#sleep 4
+#echo "running gitea entry"
 
-/bin/bash /opt/bin/gitea_entry.sh&
-
+#/bin/bash /opt/bin/gitea_entry.sh&
+supervisord -c /etc/supervisor/supervisord.conf
 
 exec "$@"
