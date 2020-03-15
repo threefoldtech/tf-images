754f37e9d7182158af669c5dfa220a3ee13c729d
Pishoy <pishoyabdo@gmail.com>
2020-03-15 10:35:53 +0200

    Update restic_backup.sh

--- discourse/restic_backup.sh
+++ discourse/restic_backup.sh
@@ -4,4 +4,4 @@ if ! restic snapshots ;then echo restic repo does not initalized yet; restic ini
 cd $app_directory
 for i in `find $app_directory -type f -mtime -1` ; do restic backup --cleanup-cache $i ; done
 #Delete files older than 7 days
-find $app_directory/ -mtime +7 -exec rm {} \;
+find $app_directory/ -type f -mtime +7 -exec rm {} \;
