0987c897db9d19d077924769090eab71c72bca5c
Bishoy <abdelmab@codescalers.com>
2020-08-09 12:10:34 +0200

    refactoring files

--- mastodon/config/initializers/open_uri_redirection.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-
-module OpenURI
-  def self.redirectable?(uri1, uri2) # :nodoc:
-    uri1.scheme.casecmp(uri2.scheme).zero? ||
-      (/\A(?:http|https|ftp)\z/i =~ uri1.scheme && /\A(?:http|https|ftp)\z/i =~ uri2.scheme)
-  end
-end
