07014f54226b8da3b8f4592f7563e428e5d8f726
Bishoy <abdelmab@codescalers.com>
2020-08-09 12:10:34 +0200

    refactoring files

--- mastodon/app/serializers/rest/featured_tag_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class REST::FeaturedTagSerializer < ActiveModel::Serializer
-  attributes :id, :name, :statuses_count, :last_status_at
-
-  def id
-    object.id.to_s
-  end
-end
--- mastodon/app/services/after_unallow_domain_service.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AfterUnallowDomainService < BaseService
-  def call(domain)
-    Account.where(domain: domain).find_each do |account|
-      SuspendAccountService.new.call(account, reserve_username: false)
-    end
-  end
-end
