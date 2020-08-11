dcc784077e80f9827d7c993aed7e818de12b9c71
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2020-08-11 23:35:45 +0000

    Fixes nginx stop
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- nginx-certbot/startup.sh
+++ nginx-certbot/startup.sh
@@ -3,16 +3,15 @@ echo $PUBKEY > ~/.ssh/authorized_keys
 service ssh start
 trc -local 127.0.0.1:80 -local-tls 127.0.0.1:443 -remote $TFGATEWAY & 
 while ! nc -z $SOLUTION_IP $SOLUTION_PORT </dev/null; do sleep 1; done
-certbot --nginx -n -m "$EMAIL" --agree-tos  --domains "$DOMAIN" 
-rm -f /etc/nginx/sites-enabled/default
-if [ "$ENFORCE_HTTPS" = 'true' ] ; then
-    cp /config/nginx-https.conf /etc/nginx/sites-enabled/default
-else
-	cp /config/nginx.conf /etc/nginx/sites-enabled/default
-fi
+cp /config/nginx.conf /etc/nginx/sites-enabled/default
 sed -i "s/DOMAIN/$DOMAIN/g" /etc/nginx/sites-enabled/default
 sed -i "s/SOLUTION_IP/$SOLUTION_IP/g" /etc/nginx/sites-enabled/default
 sed -i "s/SOLUTION_PORT/$SOLUTION_PORT/g" /etc/nginx/sites-enabled/default
-certbot --domains $DOMAIN --non-interactive --redirect --authenticator standalone --installer nginx --post-hook "service nginx start"
-service nginx reload
+if [ "$ENFORCE_HTTPS" = 'true' ] ; then
+  certbot --nginx --agree-tos  -m "$EMAIL" --non-interactive --domains $DOMAIN --redirect
+else
+  certbot --nginx --agree-tos  -m "$EMAIL" --non-interactive --domains $DOMAIN --no-redirect
+fi
+service nginx stop
+nginx -g 'daemon off;'
 
