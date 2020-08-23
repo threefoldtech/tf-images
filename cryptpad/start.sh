c7e829c241ed84de4fa4c7f57cb61afa1cb6d1ce
Bishoy <abdelmab@codescalers.com>
2020-08-23 09:41:16 +0200

    fix all entry point for hosts file and pub_key env variable

--- cryptpad/start.sh
+++ cryptpad/start.sh
@@ -4,8 +4,26 @@ mkdir -p /var/run/sshd
 chmod 600 ~/.ssh
 chmod 600 /etc/ssh/*
 touch ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-echo $pub_key >> ~/.ssh/authorized_keys
+# fix /etc/hosts
+if ! grep -q "localhost" /etc/hosts; then
+	touch /etc/hosts
+	chmod  644 /etc/hosts
+	echo $HOSTNAME  localhost >> /etc/hosts
+	echo "127.0.0.1 localhost" >> /etc/hosts
+fi
+#  check pub key
+if [ -z ${pub_key+x} ]; then
+
+        echo pub_key does not set in env variables
+else
+
+        [[ -d /root/.ssh ]] || mkdir -p /root/.ssh
+
+				if ! grep -q "$pub_key" /root/.ssh/authorized_keys; then
+					echo $pub_key >> /root/.ssh/authorized_keys
+				fi
+fi
+
 chmod 600 ~/.ssh/authorized_keys
 service ssh start
 cd /cryptpad/ && node server
