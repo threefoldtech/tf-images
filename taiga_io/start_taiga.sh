3fe387b2c2744d7658f6f0a18696527e1c56553f
Waleed <waleed.hammam@gmail.com>
2020-10-19 10:35:51 +0200

    try to stop rabbitmq before starting with supervisor

--- taiga_io/start_taiga.sh
+++ taiga_io/start_taiga.sh
@@ -72,8 +72,8 @@ su taiga -c 'bash /.prepare_taiga.sh'
 echo "Run supervisord"
 supervisord -c /etc/supervisor/supervisord.conf
 
-echo "wait 15 seconds for rabbitmq starting"
-sleep 15
+echo "wait 60 seconds for rabbitmq starting"
+sleep 60
 rabbitmqctl add_user taiga $SECRET_KEY
 rabbitmqctl add_vhost taiga
 rabbitmqctl set_permissions -p taiga taiga '.*' '.*' '.*'
