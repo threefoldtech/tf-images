7ec6bb9f609bb31cdd31ee1a8d6e5f22c73d197b
Pishoy <pishoyabdo@gmail.com>
2021-02-17 13:20:33 +0200

    Update backup.sh

--- freeflowpages/backup.sh
+++ freeflowpages/backup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export AWS_ACCESS_KEY_ID=our_aws_access_key
 export AWS_SECRET_ACCESS_KEY=our_aws_secret_key
 export RESTIC_REPOSITORY=our_restic_repository
@@ -6,10 +7,14 @@ export RESTIC_PASSWORD=our_restic_password
 backup_time=`date +%Y-%m-%d_%H%M`
 humhub_dir='/var/www/html/humhub'
 cd $humhub_dir
-mysqldump -u$DB_USER -p$DB_PASS humhub > dbbackup_$backup_time.sql && find  $humhub_dir -maxdepth 1 -type f -name 'dbbackup*.gz' -delete
+mysqldump -u$root -p$ROOT_DB_PASS humhub > dbbackup_$backup_time.sql && find  $humhub_dir -maxdepth 1 -type f -name 'dbbackup*.gz' -delete
+if [ $? -ne 0 ] ; then
+    touch WARNING_DUMP_FAILED
+    echo database backup fail at `date`
+    exit 1
+fi
 gzip dbbackup_$backup_time.sql
 unset HISTFILE
-if ! restic snapshots ;then echo restic repo does not initalized yet; restic init ; fi > /dev/null
-restic backup -q $humhub_dir
-restic forget -q --prune --keep-within 2m
-
+if ! /usr/local/bin/restic snapshots ;then echo /usr/local/bin/restic repo does not initalized yet; /usr/local/bin/restic init ; fi > /dev/null
+/usr/local/bin/restic backup -q $humhub_dir
+/usr/local/bin/restic forget -q --prune --keep-within 2m
