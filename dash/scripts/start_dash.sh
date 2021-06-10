b4cad4ff5b44f763f0b55bebc341faabe02d90e5
Abdul Rehman <a.rehmans@gmail.com>
2021-06-10 11:28:49 +0400

    Update start_dash.sh

--- dash/scripts/start_dash.sh
+++ dash/scripts/start_dash.sh
@@ -2,13 +2,11 @@
 echo "127.0.0.1 localhost" >> /etc/hosts
 if [ -z "${rpcuser}" ]; then rpcuser="tfnow"; fi
 if [ -z "${rpcpasswd}" ]; then rpcpasswd="tfdash2020"; fi
-if [ -z "${node_ingress_ip}" ]; then node_ingress_ip="VDC_IP"; fi
-echo $node_ingress_ip > /tmp/checkip
 sed -i "s/.*rpcuser=.*/rpcuser=$rpcuser/" /opt/dash.conf
 sed -i "s/.*rpcpassword=.*/rpcpassword=$rpcpasswd/" /opt/dash.conf
 mv /opt/dash.conf /dash
 cat /opt/cronjobs | crontab -
-rm -f /var/www/html/index.html && rm -f /var/www/html/.git
+rm -f /var/www/html/index.html 
 /etc/init.d/cron start
 /opt/dash/bin/dashd -conf=/dash/dash.conf > /dev/null 2>&1
 ln -s /dash/debug.log /var/www/html/node.log && chmod 777 /var/www/html/node.log
