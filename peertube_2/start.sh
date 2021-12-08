6dd11a789adc96a93f3ede31f9b76b3ea346cb82
Omarabdul3ziz <omarabdul3ziz@gmail.com>
2021-12-08 02:41:45 +0200

    expose service running on 0.0.0.0:9000

--- peertube_2/start.sh
+++ peertube_2/start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+service nginx restart
 service redis-server restart
 service postgresql restart
 service peertube start
+
+echo ">> All service Up."
+echo ">> Sleep for debugging...."
 
 sleep infinity
