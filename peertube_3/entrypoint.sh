992b8a742fb711d26251dad6cdebf27b2525b585
omarabdul3ziz <omarabdul3ziz@gmail.com>
2021-12-13 18:55:38 +0000

    almost full build

--- peertube_3/entrypoint.sh
+++ peertube_3/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-# Only execute if root ( start services )
-export PATH=$PATH:/usr/local/bin
-if  [ `id -u` = 0 ]; then 
-    service ssh start
-    chmod 0640 /etc/ssl/private/ssl-cert-snakeoil.key
-    echo "127.0.0.1 localhost" >> /etc/hosts
-    service postgresql start
-    redis-server --daemonize yes
-    if ! grep -q "$SSH_KEY" /root/.ssh/authorized_keys; then
-    	echo $SSH_KEY >> /root/.ssh/authorized_keys
-    fi
+
+if ! grep -q "$SSH_KEY" /root/.ssh/authorized_keys; then
+    echo $SSH_KEY >> /root/.ssh/authorized_keys
 fi
 
-# run peertube
+service ssh start
+
+
+export PATH=$PATH:/usr/local/bin
+chmod 0640 /etc/ssl/private/ssl-cert-snakeoil.key
+echo "127.0.0.1 localhost" >> /etc/hosts
+
+service postgresql start
+redis-server --daemonize yes
+
 if [ -z "$(ls -A /config)" ]; then
     cp /app/support/docker/production/config/* /config
 fi
@@ -21,18 +22,7 @@ fi
 # Always copy default and custom env configuration file, in cases where new keys were added
 cp /app/config/default.yaml /config
 cp /app/support/docker/production/config/custom-environment-variables.yaml /config
-#find /config ! -user peertube -exec chown peertube:peertube {} \;
-# first arg is `-f` or `--some-option`
-# or first arg is `something.conf`
-#if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
-#    set -- npm "$@"
-#fi
 
-# allow the container to be started with `--user`
-#if [ "$1" = 'npm' -a "$(id -u)" = '0' ]; then
-#    find /data ! -user peertube -exec  chown peertube:peertube {} \;
-#    exec gosu peertube "$0" "$@"
-#fi
-#exec "$@"
 cd /app
-npm start
+NODE_CONFIG_DIR=/config NODE_ENV=production npm start
+
--- peertube_3/startup.sh
+++
@@ -1,8 +0,0 @@
-service ssh start
-service postgresql start
-redis-server --daemonize yes
-echo $PUB_KEY
-if ! grep -q "$PUB_KEY" /root/.ssh/authorized_keys; then
-	echo $PUB_KEY >> /root/.ssh/authorized_keys
-fi
-npm start
