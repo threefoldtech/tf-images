6f0d19731c8c143d2d0dfe202adb44202d2110f1
omarabdul3ziz <omarabdul3ziz@gmail.com>
2021-12-14 12:38:01 +0000

    more abillity to config

--- peertube_4/entrypoint.sh
+++ peertube_4/entrypoint.sh
@@ -14,49 +14,51 @@ fi
 
 service ssh start
 
-
 # Redis server
-if [ ! -z $PEERTUBE_REDIS_HOSTNAME ]; then 
-    if ! grep -q "bind $PEERTUBE_REDIS_HOSTNAME" /etc/redis/redis.conf; then
-    echo "bind $PEERTUBE_REDIS_HOSTNAME" >> /etc/redis/redis.conf
-    fi
-fi
+export PEERTUBE_REDIS_HOSTNAME=127.0.0.1
+export PEERTUBE_REDIS_PORT=6379
 
-if [ ! -z $PEERTUBE_REDIS_HOSTNAME ]; then 
-    if ! grep -q "port $PEERTUBE_REDIS_PORT" /etc/redis/redis.conf; then
-    echo "port $PEERTUBE_REDIS_PORT" >> /etc/redis/redis.conf
-    fi
-fi
-
-if [ ! -z $PEERTUBE_REDIS_HOSTNAME ]; then 
-    if ! grep -q "requirepass $PEERTUBE_REDIS_AUTH" /etc/redis/redis.conf; then
+if [ ! -z $PEERTUBE_REDIS_AUTH ]; then 
     echo "requirepass $PEERTUBE_REDIS_AUTH" >> /etc/redis/redis.conf
-    fi
 fi
-
+echo "Starting Redis Server."
 redis-server /etc/redis/redis.conf
 
-# service postgresql start &&\
-#     su postgres -c "psql --file=/tmp/setup_postgres.sql"
+# PSQL server
+export PEERTUBE_DB_HOSTNAME=127.0.0.1
+export PEERTUBE_DB_PORT=5432
 
+chmod 0640 /etc/ssl/private/ssl-cert-snakeoil.key
+export PATH=$PATH:/usr/local/bin
+echo "127.0.0.1 localhost" >> /etc/hosts
 
+service postgresql start
 
-# export PATH=$PATH:/usr/local/bin
-# chmod 0640 /etc/ssl/private/ssl-cert-snakeoil.key
-# echo "127.0.0.1 localhost" >> /etc/hosts
+touch /tmp/init_postgres.sql
 
-# service postgresql start
-# redis-server --daemonize yes
+cat <<EOF > /tmp/init_postgres.sql
+create database peertube$PEERTUBE_DB_SUFFIX;
+create user $PEERTUBE_DB_USERNAME password '$PEERTUBE_DB_PASSWORD';
+grant all privileges on database peertube$PEERTUBE_DB_SUFFIX to $PEERTUBE_DB_USERNAME;
+\c peertube$PEERTUBE_DB_SUFFIX
+CREATE EXTENSION pg_trgm;
+CREATE EXTENSION unaccent;
+EOF
 
-# if [ -z "$(ls -A /config)" ]; then
-#     cp /app/support/docker/production/config/* /config
-# fi
+su postgres -c "psql --file=/tmp/init_postgres.sql"
 
-# # Always copy default and custom env configuration file, in cases where new keys were added
-# cp /app/config/default.yaml /config
-# cp /app/support/docker/production/config/custom-environment-variables.yaml /config
+# Peertube server
 
-# cd /app
-# NODE_CONFIG_DIR=/config NODE_ENV=production
+# default configs
+cp /app/config/default.yaml /config
 
-sleep infinity
+# overriden by productions configs
+cp /app/support/docker/production/config/production.yaml /config
+
+# schema to overriden by environment variables
+cp /app/support/docker/production/config/custom-environment-variables.yaml /config
+
+# Start peertube server
+cd /app
+NODE_CONFIG_DIR=/config NODE_ENV=production npm start
+# sleep infinity
