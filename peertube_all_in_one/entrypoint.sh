ca80420f30fb68a42f0e81a8c379869198effa9a
omarabdul3ziz <omarabdul3ziz@gmail.com>
2021-12-16 10:27:59 +0000

    listen on ipv6 as default

--- peertube_all_in_one/entrypoint.sh
+++ peertube_all_in_one/entrypoint.sh
@@ -58,8 +58,8 @@ cp /app/support/docker/production/config/production.yaml /config
 # schema to overriden by environment variables
 cp /app/support/docker/production/config/custom-environment-variables.yaml /config
 
-# allow peertube to run on IPv4 or IPv6 interface according to BIND var
-sed -i "0,/hostname: '0.0.0.0'/ s//hostname: '$PEERTUBE_BIND_ADDRESS'/" /config/production.yaml
+# Listen on ipv6
+sed -i "0,/hostname: '0.0.0.0'/ s//hostname: '::'/" /config/production.yaml
 
 # Start peertube server
 cd /app
