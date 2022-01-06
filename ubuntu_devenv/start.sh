94ab90f2e833f0ba54d11f103f62c11567dc15f0
Your Name <you@example.com>
2022-01-06 22:39:02 +0000

    fix exporting go path

--- ubuntu_devenv/start.sh
+++ ubuntu_devenv/start.sh
@@ -7,8 +7,11 @@ service ssh start
 
 service redis-server start
 
+echo '
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin
+' >> ~/.profile
+
 
 sleep infinity
