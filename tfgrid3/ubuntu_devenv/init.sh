800fcc23ff31de59e08668f8d0fa177b4308520e
MElborolossy <mohammedelborolossy@gmail.com>
2022-01-12 19:34:15 +0200

    - Build poc2 weblet in DockerFile - Run vserver, VWeblet and msgbusd if twinId passed as ENV

--- tfgrid3/ubuntu_devenv/init.sh
+++ tfgrid3/ubuntu_devenv/init.sh
@@ -12,6 +12,22 @@ chmod 600 ~/.ssh/authorized_keys
 
 service redis-server start
 
+# "Run vserver, logs located in: /var/log/vserver"
+vserver &>> /var/log/vserver &
+
+# "Run VWeblet"
+pushd /root/weblets_pocs/poc2/VWeblet
+npm run vtwin &>> /var/log/vtwin &
+popd
+
+if [[-z "$TWIN_ID" ]] ;
+then
+# "Can't run msgbusd, you can run it manaully later."
+else
+    echo "Run msgbusd with twinId $TWIN_ID on devnet"
+    msgbusd --twin $TWIN_ID &>> /var/log/msgbusd &
+fi
+
 echo '
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
