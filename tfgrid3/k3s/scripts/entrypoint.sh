cae338706afbf4d2df3515e45eb5d28588cbbd01
Sameh-farouk <samehabouelsaad@gmail.com>
2023-01-05 18:57:23 +0200

    switch back to cp command

--- tfgrid3/k3s/scripts/entrypoint.sh
+++ tfgrid3/k3s/scripts/entrypoint.sh
@@ -2,7 +2,7 @@ echo "KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /etc/environment
 
 if [ ! -z "${K3S_DATA_DIR}" ]; then
     echo "k3s data-dir set to: $K3S_DATA_DIR"
-    mv -r /var/lib/rancher/k3s/* $K3S_DATA_DIR
+    cp -r /var/lib/rancher/k3s/* $K3S_DATA_DIR && rm -rf /var/lib/rancher/k3s
     EXTRA_ARGS="--data-dir $K3S_DATA_DIR --kubelet-arg=root-dir=$K3S_DATA_DIR/kubelet"
 fi
 
