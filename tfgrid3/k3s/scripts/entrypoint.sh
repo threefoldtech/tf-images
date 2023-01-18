e730788be4719a699a01e38cd0b3f26ee00020f9
sameh-farouk <samehabouelsaad@gmail.com>
2023-01-18 18:39:37 +0200

    set KUBECONFIG environment value in the Dockerfile iss#99

--- tfgrid3/k3s/scripts/entrypoint.sh
+++ tfgrid3/k3s/scripts/entrypoint.sh
@@ -1,5 +1,3 @@
-echo "KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /etc/environment
-
 if [ ! -z "${K3S_DATA_DIR}" ]; then
     echo "k3s data-dir set to: $K3S_DATA_DIR"
     cp -r /var/lib/rancher/k3s/* $K3S_DATA_DIR && rm -rf /var/lib/rancher/k3s
