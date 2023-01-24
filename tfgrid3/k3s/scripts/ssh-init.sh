c81444bf2457ae880408dd14d39b374466a535b1
sameh-farouk <samehabouelsaad@gmail.com>
2023-01-25 00:13:50 +0200

    Fix wrong script name

--- tfgrid3/k3s/scripts/ssh-init.sh
+++ tfgrid3/k3s/scripts/ssh-init.sh
@@ -5,10 +5,9 @@ set -x
 if [ ! -z "$SSH_KEY" ]; then
     mkdir -p /var/run/sshd
     mkdir -p /root/.ssh
-    touch /root/.ssh/authorized_keys
     
     chmod 700 /root/.ssh
-    chmod 600 /root/.ssh/authorized_keys
 
-    echo "$SSH_KEY" >> /root/.ssh/authorized_keys
+    echo "$SSH_KEY" > /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
 fi
