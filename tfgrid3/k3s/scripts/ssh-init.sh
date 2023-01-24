f525bd68f06a7559bb5b4d7d0b5600956a1a71ef
sameh-farouk <samehabouelsaad@gmail.com>
2023-01-24 23:48:01 +0200

    fixing sshd not started

--- tfgrid3/k3s/scripts/ssh-init.sh
+++ tfgrid3/k3s/scripts/ssh-init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-mkdir -p /var/run/sshd
-mkdir -p /root/.ssh/ 
-echo $SSH_KEY > /root/.ssh/authorized_keys
+set -x
+
+if [ ! -z "$SSH_KEY" ]; then
+    mkdir -p /var/run/sshd
+    mkdir -p /root/.ssh
+    touch /root/.ssh/authorized_keys
+    
+    chmod 700 /root/.ssh
+    chmod 600 /root/.ssh/authorized_keys
+
+    echo "$SSH_KEY" >> /root/.ssh/authorized_keys
+fi
