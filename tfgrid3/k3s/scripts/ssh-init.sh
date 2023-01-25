f9bed027105cca22f28cb93360588b37f55411b9
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2023-01-25 13:02:48 +0200

    Development k3s upgrade (#122)
    * use default k3s traefik and apply custom config iss#121
    
    * Update Dockerfile
    
    * remove curl
    
    * remove kubectl binary from repo
    
    * modifiy zinit service & fix log
    
    * remove wget '--no-check-certificate' option
    
    * Update flist url
    
    * remove saving k3s logs to file as we use zinit/kernel log buffer
    
    * Adding additional SANs to tls cert iss#98
    
    * Remove unnecessary call to shell
    
    * set the default shell in scripts
    
    * export KUBECONFIG at shell login  iss#99

--- tfgrid3/k3s/scripts/ssh-init.sh
+++ tfgrid3/k3s/scripts/ssh-init.sh
@@ -1,3 +1,13 @@
-mkdir -p /var/run/sshd
-mkdir -p /root/.ssh/ 
-echo $SSH_KEY > /root/.ssh/authorized_keys
+#!/bin/bash
+
+set -x
+
+if [ ! -z "$SSH_KEY" ]; then
+    mkdir -p /var/run/sshd
+    mkdir -p /root/.ssh
+    
+    chmod 700 /root/.ssh
+
+    echo "$SSH_KEY" > /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+fi
