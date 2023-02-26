f71c6381e0b3a95a482f395ffe018b1fcd5779ff
Thunder <mahmmoud.hassanein@gmail.com>
2023-02-26 15:51:31 +0200

    WIP: sync the code to pull it inside the container.

--- tfgrid3/mastodon/scripts/mastodon.sh
+++ tfgrid3/mastodon/scripts/mastodon.sh
@@ -7,9 +7,9 @@ if [ -z "$SUPERUSER_USERNAME" ] || [ -z "$SUPERUSER_EMAIL" ] || [ -z "$SUPERUSER
         exit 1
 else
     # Create and migrate the database.
-    docker-compose run --rm web bundle exec rails db:setup
+    docker compose run --rm web bundle exec rails db:setup
     # Create admin user with given email
-    RAILS_ENV=production docker-compose run --rm web bundle exec rails r ruby_scripts/create_superuser.rb
-    RAILS_ENV=production docker-compose run --rm web bundle exec rails r ruby_scripts/create_relays.rb
-    docker-compose up
+    RAILS_ENV=production docker compose run --rm web bundle exec rails r ruby_scripts/create_superuser.rb
+    RAILS_ENV=production docker compose run --rm web bundle exec rails r ruby_scripts/create_relays.rb
+    docker compose up
 fi
