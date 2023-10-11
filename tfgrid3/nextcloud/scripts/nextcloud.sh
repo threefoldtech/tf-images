1ca646ef1a8d80f4cb2814f20526d8d5da436dc1
Scott Yeager <yeagersm@gmail.com>
2023-10-11 12:30:39 -0700

    Nextcloud gateway support and docker data on disk

--- tfgrid3/nextcloud/scripts/nextcloud.sh
+++ tfgrid3/nextcloud/scripts/nextcloud.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-echo "Before docker info sleep loop." >> /usr/local/bin/nextcloud_installation.md
-
 export COMPOSE_HTTP_TIMEOUT=800
-set -x
 while ! docker info > /dev/null 2>&1; do
+    echo docker not ready
     sleep 2
 done
 
-echo "After docker info sleep loop." >> /usr/local/bin/nextcloud_installation.md
-
 docker run \
+--init \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
 --restart always \
---publish 80:80 \
+--publish 8000:8000 \
 --publish 8080:8080 \
---publish 8443:8443 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--env SKIP_DOMAIN_VALIDATION=true \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest
