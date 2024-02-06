b03f5a74861645c25e29854df84a41a37758f36b
khaledyoussef24 <mahmoudk@incubaid.com>
2024-02-06 14:20:30 +0200

    fixes for the script

--- tfgrid3/gitea/setup-gitea.sh
+++ tfgrid3/gitea/setup-gitea.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Install SQLite
-apt update
-apt install -y sqlite3
 
 # Create a system user to run Gitea
 adduser --system --shell /bin/bash --gecos 'Git Version Control' --group --disabled-password --home /home/git git
 
 # Download the latest Gitea binary
-wget -O gitea https://dl.gitea.io/gitea/1.15.5/gitea-1.15.5-linux-amd64
+wget -O /usr/local/bin/gitea https://dl.gitea.io/gitea/1.15.5/gitea-1.15.5-linux-amd64
 
 # Make the Gitea binary executable
-chmod +x gitea
+chmod +x /usr/local/bin/gitea
 
 # Create the required directory structure
 mkdir -p /var/lib/gitea/{custom,data,log}
@@ -21,11 +18,9 @@ mkdir /etc/gitea
 chown root:git /etc/gitea
 chmod 770 /etc/gitea
 
-# Initialize the Gitea configuration file
+# # Initialize the Gitea configuration file
 sudo -u git ./gitea web -c /etc/gitea/app.ini 
 
-# Create the Gitea SQLite database
-sudo -u git ./gitea migrate -c /etc/gitea/app.ini
+# # Create the Gitea SQLite database
+# sudo -u git ./gitea migrate -c /etc/gitea/app.ini
 
-# Start the Gitea service
-sudo -u git ./gitea web -c /etc/gitea/app.ini
