ab39d308493f589fd68d39084cf289812bbe2458
PeterNashaat <peter@codescalers.com>
2024-05-01 13:37:09 +0000

    adding tls

--- tfgrid3/funkwhale/scripts/funkwhale_init.sh
+++ tfgrid3/funkwhale/scripts/funkwhale_init.sh
@@ -11,3 +11,20 @@ sudo -u postgres psql $FUNKWHALE_DB_NAME -c 'CREATE EXTENSION "citext";'
 sudo -u funkwhale /srv/funkwhale/venv/bin/funkwhale-manage migrate
 sudo -u funkwhale /srv/funkwhale/venv/bin/funkwhale-manage fw users create --superuser --username $SUPERUSER_USERNAME --email $SUPERUSER_EMAIL --password $SUPERUSER_PASSWORD
 sudo -u funkwhale /srv/funkwhale/venv/bin/funkwhale-manage collectstatic
+
+curl -L -o /etc/nginx/funkwhale_proxy.conf "https://dev.funkwhale.audio/funkwhale/funkwhale/raw/1.4.0/deploy/funkwhale_proxy.conf"
+curl -L -o /etc/nginx/sites-available/funkwhale.template "https://dev.funkwhale.audio/funkwhale/funkwhale/raw/1.4.0/deploy/nginx.template"
+
+set -a && . /srv/funkwhale/config/.env && set +a
+envsubst "`env | awk -F = '{printf \" $%s\", $$1}'`" \
+	   < /etc/nginx/sites-available/funkwhale.template \
+	      > /etc/nginx/sites-available/funkwhale.conf
+
+grep '${' /etc/nginx/sites-available/funkwhale.conf
+
+
+ln -s /etc/nginx/sites-available/funkwhale.conf /etc/nginx/sites-enabled/
+
+
+sudo certbot --nginx -d $FUNKWHALE_HOSTNAME --non-interactive --agree-tos --email $SUPERUSER_EMAIL --redirect
+
