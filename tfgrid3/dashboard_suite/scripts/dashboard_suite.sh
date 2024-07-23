c2f4e9cea620b55cc7dd5369b6f7ef8bdd765718
Mik-TF <logismos@protonmail.ch>
2024-07-23 11:08:40 -0400

    updated script for dashboard suite with yes answers

--- tfgrid3/dashboard_suite/scripts/dashboard_suite.sh
+++ tfgrid3/dashboard_suite/scripts/dashboard_suite.sh
@@ -9,11 +9,11 @@ cp .secrets.env-example .secrets.env
 # Set the secret key
 KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 18; echo)
 
-# Set variables in file
+# Set variables in secret env file
 sed -i "s/DOMAIN\=/DOMAIN\=${DOMAIN}/g" .secrets.env
 sed -i "s/TFCHAIN_NODE_KEY\=/TFCHAIN_NODE_KEY\=${KEY}/g" .secrets.env
 sed -i "s/ACTIVATION_SERVICE_MNEMONIC\=\"\"/ACTIVATION_SERVICE_MNEMONIC\=\"${SEED}\"/g" .secrets.env
 sed -i "s/GRID_PROXY_MNEMONIC\=\"\"/GRID_PROXY_MNEMONIC\=\"${SEED}\"/g" .secrets.env
 
 # Load and deploy the TFGrid Stack
-sh install_grid_bknd.sh
+yes y | sh install_grid_bknd.sh
