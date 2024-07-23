5d1e62e34a7630cacf1da0c8f7181314729a1753
Mik-TF <logismos@protonmail.ch>
2024-07-23 15:08:13 -0400

    updated script to deploy grid backend

--- tfgrid3/dashboard_suite/scripts/dashboard_suite.sh
+++ tfgrid3/dashboard_suite/scripts/dashboard_suite.sh
@@ -6,10 +6,13 @@ cd /code/grid_deployment/docker-compose/${NETWORK}net
 # Copy the secret env file
 cp .secrets.env-example .secrets.env
 
-# Set the secret key
-KEY=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 18; echo)
+# Create a subkey and store into KEY variable, with .gitignore exception
+grep -qxF ".subkey_${NETWORK}net" .gitignore || echo -e "\n.subkey_${NETWORK}net" >> .gitignore
+../../apps/subkey generate-node-key > .subkey_${NETWORK}net
+KEY=$(cat .subkey_${NETWORK}net)
 
 # Set variables in secret env file
+sed -i "s/DOMAIN\=grid.tf/example.com/g" .secrets.env
 sed -i "s/DOMAIN\=/DOMAIN\=${DOMAIN}/g" .secrets.env
 sed -i "s/TFCHAIN_NODE_KEY\=/TFCHAIN_NODE_KEY\=${KEY}/g" .secrets.env
 sed -i "s/ACTIVATION_SERVICE_MNEMONIC\=\"\"/ACTIVATION_SERVICE_MNEMONIC\=\"${SEED}\"/g" .secrets.env
