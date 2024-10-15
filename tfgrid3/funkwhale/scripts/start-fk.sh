71aeb91691c7bb243e3fe03aaf6da583f1318212
PeterNashaat <peternashaaat@gmail.com>
2024-10-15 13:45:15 +0000

    Adding fuw and funkwhale start script

--- tfgrid3/funkwhale/scripts/start-fk.sh
+++ tfgrid3/funkwhale/scripts/start-fk.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -x
+
+# Check if FUNKWHALE_VERSION is set, if not, assign a default value
+FUNKWHALE_VERSION=${FUNKWHALE_VERSION:-1.4.0}
+
+DJANGO_SECRET=$(openssl rand -base64 45)
 
 # Create funkwhale system user
-sudo useradd --system --shell /bin/bash --create-home --home-dir /srv/funkwhale funkwhale
+useradd --system --shell /bin/bash --create-home --home-dir /srv/funkwhale funkwhale
 
 # Change to Funkwhale directory
 cd /srv/funkwhale/
@@ -10,37 +16,33 @@ cd /srv/funkwhale/
 curl -L -o /srv/funkwhale/docker-compose.yml "https://dev.funkwhale.audio/funkwhale/funkwhale/raw/${FUNKWHALE_VERSION}/deploy/docker-compose.yml"
 curl -L -o /srv/funkwhale/.env "https://dev.funkwhale.audio/funkwhale/funkwhale/raw/${FUNKWHALE_VERSION}/deploy/env.prod.sample"
 
-# Update the FUNKWHALE_VERSION in the .env file
-sed -i "s/FUNKWHALE_VERSION=latest/FUNKWHALE_VERSION=$FUNKWHALE_VERSION/" .env
-
 # Set appropriate permissions for the .env file
 chmod 600 /srv/funkwhale/.env
 
 # Generate a random Django secret key and update the .env file
-DJANGO_SECRET=$(openssl rand -base64 45)
-sed -i "/DJANGO_SECRET_KEY=/DJANGO_SECRET_KEY=$DJANGO_SECRET/" .env
+sed -i "s#^DJANGO_SECRET_KEY=.*#DJANGO_SECRET_KEY=$DJANGO_SECRET#" /srv/funkwhale/.env
 
 # Pull the latest Docker images for Funkwhale
-docker compose pull
+docker-compose pull
 
 # Start PostgreSQL service
-docker compose up -d postgres
+docker-compose up -d postgres
 
 # Run database migrations
-docker compose run --rm api funkwhale-manage migrate
+docker-compose run --rm api funkwhale-manage migrate
 
 # Create a superuser using the provided credentials
-docker compose run --rm -T api funkwhale-manage fw users create --superuser << EOF
+docker-compose run --rm -T api funkwhale-manage fw users create --superuser << EOF
 $FUNKWHALE_SUPERUSER_NAME
 $FUNKWHALE_SUPERUSER_PASSWORD
 $FUNKWHALE_SUPERUSER_EMAIL
 EOF
 
 # Start Funkwhale services
-docker compose up -d
+docker-compose up -d
 
 # Configure temporary Nginx config for Certbot verification
-cat <<EOF | sudo tee /etc/nginx/sites-available/funkwhale
+cat <<EOF | tee /etc/nginx/sites-available/funkwhale.conf
 server {
     listen 80;
     server_name funkwhale.peter.ourworld.tf;
@@ -56,10 +58,13 @@ server {
 EOF
 
 # Enable the temporary Nginx site
-ln -s /etc/nginx/sites-available/funkwhale /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/funkwhale.conf /etc/nginx/sites-enabled/
 
 # Generate SSL certificate using Certbot
-certbot --nginx -d $Domain --non-interactive --agree-tos --register-unsafely-without-email --redirect
+certbot --nginx -d $Domain --non-interactive --agree-tos --register-unsafely-without-email --redirect --nginx-server-root /etc/nginx/sites-available/funkwhale.conf
+
+# Cleaining up tmp config files
+rm -rf /etc/nginx/sites-available/funkwhale.conf /etc/nginx/sites-enabled/funkwhale.conf
 
 # Download and apply the Funkwhale Nginx proxy configuration
 curl -L -o /etc/nginx/funkwhale_proxy.conf "https://dev.funkwhale.audio/funkwhale/funkwhale/raw/$FUNKWHALE_VERSION/deploy/funkwhale_proxy.conf"
@@ -69,9 +74,13 @@ curl -L -o /etc/nginx/sites-available/funkwhale.template "https://dev.funkwhale.
 set -a && source /srv/funkwhale/.env && set +a
 envsubst "`env | awk -F = '{printf \" $%s\", $$1}'`" < /etc/nginx/sites-available/funkwhale.template > /etc/nginx/sites-available/funkwhale.conf
 
+# Adding Domain to nginx config file
+sed -i "s/server_name yourdomain.funkwhale;/server_name $Domain;/g" /etc/nginx/sites-available/funkwhale.conf
+sed -i "s|/etc/letsencrypt/live/yourdomain.funkwhale/fullchain.pem|/etc/letsencrypt/live/$Domain/fullchain.pem|g" /etc/nginx/sites-available/funkwhale.conf
+sed -i "s|/etc/letsencrypt/live/yourdomain.funkwhale/privkey.pem|/etc/letsencrypt/live/$Domain/privkey.pem|g" /etc/nginx/sites-available/funkwhale.conf
+
 # Enable the final Funkwhale Nginx site
 ln -s /etc/nginx/sites-available/funkwhale.conf /etc/nginx/sites-enabled/
-
 
 # Reload Nginx to apply SSL configuration
 zinit stop nginx
