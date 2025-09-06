cd46626e202e74fd1d8e614ece39db8d06b4d386
Scott Yeager <yeagersm@gmail.com>
2025-09-05 20:12:36 -0700

    Keep disabled Nextcloud apps

--- tfgrid3/nextcloud/scripts/nextcloud.sh
+++ tfgrid3/nextcloud/scripts/nextcloud.sh
@@ -12,6 +12,7 @@ docker run \
 --env APACHE_PORT=11000 \
 --env APACHE_IP_BINDING=0.0.0.0 \
 --env SKIP_DOMAIN_VALIDATION=true \
+--env NEXTCLOUD_KEEP_DISABLED_APPS=true \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest
