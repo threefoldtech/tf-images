693a6ff86175423d4330e17a3ad3283a82bfd03d
Scott Yeager <yeagersm@gmail.com>
2025-09-05 20:10:39 -0700

    Disable talk container disabling

--- tfgrid3/nextcloud/scripts/caddy.sh
+++ tfgrid3/nextcloud/scripts/caddy.sh
@@ -14,13 +14,4 @@ else
   export PORT=80
 fi
 
-if ! $IPV4; then
-  export BODY="\`<body onload=\"if (document.getElementById('talk') && document.getElementById('talk').checked) {document.getElementById('talk').checked = false; document.getElementById('options-form-submit').click()}\">\`"
-
-  export REPLACEMENTS='			`name="talk"` `name="talk" disabled`
-			`needs ports 3478/TCP and 3478/UDP open/forwarded in your firewall/router` `running the Talk container requires a public IP and this VM does not have one. It is still possible to use Talk in a limited capacity. Please consult the documentation for details`'
-else
-  export BODY="<body>"
-fi
-
 caddy run --config /etc/caddy/Caddyfile
